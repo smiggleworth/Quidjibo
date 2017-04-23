3a6caa638d150d047c2211d91541211830430e19
Jeff Repanich <jrepanich@hotmail.com>
2017-04-23 17:32:58 -0400

    wip

--- src/Quidjibo.DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Quidjibo.DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -11,22 +11,49 @@ namespace Quidjibo.DependencyInjection.Extensions
     {
         public static IServiceCollection AddQuidjibo(this IServiceCollection serviceCollection, params Assembly[] assemblies)
         {
+            var handlerType = typeof(IWorkHandler<>);
             var handlerTypes = from a in assemblies
                                from t in a.GetTypes()
-                               where t.IsClosedGenericOf(typeof(IWorkHandler<>))
+                               where t.IsAssignableToGenericType(handlerType)
                                select t;
 
             foreach (var type in handlerTypes)
             {
-                serviceCollection.Add(new ServiceDescriptor(typeof(IWorkHandler<>), type, ServiceLifetime.Transient));
+                var interfaceType = type.GetTypeInfo().ImplementedInterfaces.First(t => handlerType.GetTypeInfo().IsAssignableFrom(t.GetTypeInfo()));
+                var genericType = typeof(IWorkHandler<>).MakeGenericType(interfaceType.GenericTypeArguments);
+                serviceCollection.Add(new ServiceDescriptor(genericType, type, ServiceLifetime.Transient));
             }
             serviceCollection.AddSingleton<IQuidjiboClient, QuidjiboClient>();
             return serviceCollection;
         }
 
-        private static bool IsClosedGenericOf(this Type type, Type openGeneric)
+        /// <summary>
+        ///     Determines whether [is assignable to generic type] [the specified generic type].
+        ///     http://stackoverflow.com/questions/74616/how-to-detect-if-type-is-another-generic-type
+        /// </summary>
+        /// <param name="givenType">Type of the given.</param>
+        /// <param name="genericType">Type of the generic.</param>
+        /// <returns></returns>
+        private static bool IsAssignableToGenericType(this Type givenType, Type genericType)
         {
-            return false;
+            var typeInfo = givenType.GetTypeInfo();
+            var interfaceTypes = typeInfo.ImplementedInterfaces;
+            if (interfaceTypes.Any(it => it.GetTypeInfo().IsGenericType && it.GetGenericTypeDefinition() == genericType))
+            {
+                return true;
+            }
+            if (typeInfo.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            var baseType = typeInfo.BaseType;
+            if (baseType == null)
+            {
+                return false;
+            }
+
+            return IsAssignableToGenericType(baseType, genericType);
         }
     }
 }
