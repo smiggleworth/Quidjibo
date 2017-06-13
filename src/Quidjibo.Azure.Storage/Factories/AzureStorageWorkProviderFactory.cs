51d5ed7dfbbf09b63fd00e0d3fe2af128383e5c3
jeff.repanich <jeff.repanich@thycotic.com>
2017-06-13 08:47:01 -0400

    stub out AzureStorage integration

---
+++ src/Quidjibo.Azure.Storage/Factories/AzureStorageWorkProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Quidjibo.Factories;
+using Quidjibo.Providers;
+
+namespace Quidjibo.Azure.Storage.Factories
+{
+    public class AzureStorageWorkProviderFactory : IWorkProviderFactory
+    {
+        public Task<IWorkProvider> CreateAsync(string queue, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Quidjibo.Azure.Storage/Providers/AzureStorageWorkProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Quidjibo.Models;
+using Quidjibo.Providers;
+
+namespace Quidjibo.Azure.Storage.Providers
+{
+    public class AzureStorageWorkProvider : IWorkProvider
+    {
+        public Task SendAsync(WorkItem item, int delay, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<WorkItem>> ReceiveAsync(string worker, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DateTime> RenewAsync(WorkItem workItem, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteAsync(WorkItem workItem, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FaultAsync(WorkItem workItem, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
