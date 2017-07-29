e6921b1772523dfa9ada7bf8b7cd293f0ad562f1
Jeff Repanich <jrepanich@hotmail.com>
2017-07-29 09:40:55 -0400

    owin proxy server middleware wip

---
+++ src/Quidjibo.Owin/Extensions/QuidjiboAppBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.BuilderProperties;
+using Owin;
+using Quidjibo.Owin.Middleware;
+
+namespace Quidjibo.Owin.Extensions
+{
+    public static class QuidjiboAppBuilderExtensions
+    {
+        public static IAppBuilder UseQuidjibo(this IAppBuilder appBuilder, QuidjiboBuilder quidjiboBuilder)
+        {
+            var props = new AppProperties(appBuilder.Properties);
+            var server = quidjiboBuilder.BuildServer();
+            quidjiboBuilder.BuildClient();
+            props.OnAppDisposing.Register(server.Dispose);
+            server.Start();
+            return appBuilder;
+        }
+
+        public static IAppBuilder UseQuidjiboProxyServer(this IAppBuilder appBuilder, QuidjiboBuilder quidjiboBuilder)
+        {
+            appBuilder.Use<QuidjiboProxyServerMiddleware>(quidjiboBuilder);
+            return appBuilder;
+        }
+    }
+}
---
+++ src/Quidjibo.Owin/Middleware/QuidjiboProxyServerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Quidjibo.Owin.Middleware
+{
+    public class QuidjiboProxyServerMiddleware : OwinMiddleware
+    {
+
+
+        public QuidjiboProxyServerMiddleware(OwinMiddleware next, QuidjiboBuilder quidjiboBuilder) : base(next)
+        {
+            //_quidjiboBuilder = quidjiboBuilder;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
