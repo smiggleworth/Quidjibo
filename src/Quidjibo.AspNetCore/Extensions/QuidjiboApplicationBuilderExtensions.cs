2ab955b2275ef683ca2b58dfdb7941e04013d146
smiggleworth <smiggleworth@jefwork.com>
2017-11-12 11:54:11 -0500

    aspnetcore builder extensions

--- src/Quidjibo.AspNetCore/Extensions/QuidjiboApplicationBuilderExtensions.cs
+++ src/Quidjibo.AspNetCore/Extensions/QuidjiboApplicationBuilderExtensions.cs
@@ -8,15 +8,24 @@ namespace Quidjibo.AspNetCore.Extensions
 {
     public static class QuidjiboApplicationBuilderExtensions
     {
-        public static IApplicationBuilder UseQuidjibo(this IApplicationBuilder app, QuidjiboBuilder quidjiboBuilder)
+        public static IApplicationBuilder UseQuidjibo(this IApplicationBuilder app, QuidjiboBuilder builder)
         {
             var lifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
-            var server = quidjiboBuilder.BuildServer();
-            quidjiboBuilder.BuildClient();
+            var server = builder.BuildServer();
+            builder.BuildClient();
             lifetime.ApplicationStarted.Register(server.Start);
             lifetime.ApplicationStopping.Register(server.Stop);
             lifetime.ApplicationStopped.Register(server.Dispose);
             return app;
+        }
+
+        public static IApplicationBuilder UseQuidjibo(this IApplicationBuilder app, Action<QuidjiboBuilder> builder)
+        {
+            var quidjiboBuilder = new QuidjiboBuilder();
+
+            builder?.Invoke(quidjiboBuilder);
+
+            return app.UseQuidjibo(quidjiboBuilder);
         }
     }
 }
